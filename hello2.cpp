d523e70d134d36d19ac21e76121112210690e62a
bems <aryobimo@students.amikom.ac.id>
2023-02-01 20:17:38 +0700

    up praktikum 1 alprog semester 1

---
+++ hello2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	//string nama = "Aryo Bimo Wicaksono"
+	//string nim = "21.11.4311"
+	//string jurusan = "Informatika"
+	//string asal = "Sleman, Yogyakarta"
+	
+	cout<<"=============================="<<endl;
+	cout<<"         hello world         "<<endl;
+	cout<<"=============================="<<endl;
+	
+	cout<<"Nama			:"<<"Aryo Bimo Wicaksono"<<endl;
+	cout<<"NIM			:"<<"21.11.4311"<<endl;
+	cout<<"Jurusan			:"<<"Informatika"<<endl;
+	cout<<"Asal			:"<<"Sleman, Yogyakarta"<<endl;
+}
